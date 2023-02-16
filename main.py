704f7d4b962fde9f4796d2a912ec297f5ae7b12d
FranciaPaV <francia.pareja@gmail.com>
2023-02-16 08:23:17 -0500

    nt2 se modifica main con f prints y se crea archivo de condicionales

---
+++ main.py
@@ -0,0 +1,18 @@
+#datos primitivos - quemados - hardcoreados
+nombreUsuario= "Francia"
+edadUsuario=46
+estaturaUsuario=1.71
+yaDesayunaste=False
+direccionUsuario=input("Digita tu direccion:")
+
+#Salidas en consola
+print("Hola, tu nombre es"+nombreUsuario+" Bienvenido!!")
+print("Tu edad es: ",edadUsuario, " estas viejito!!")
+print(f"Tu direccion es: {direccionUsuario} vives cerca.")
+
+numero1=int(input("Ingresa un numero:"))
+numero2=int(input("Ingresa otro numero:"))
+
+sumatoria=numero1+numero2
+
+print(f"la suma de los dos numeros es: {sumatoria}")
